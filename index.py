50d044bef13af0b5c914c5e83e3df21deb173c7d
Akash0902 <115146885+Akash0902@users.noreply.github.com>
2024-01-05 21:09:31 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,36 @@
+from chatterbot import ChatBot 
+from chatterbot.trainers import  ChatterBotCorpusTrainer
+from flask import Flask, render_template , request
+
+app = Flask(__name__)
+
+bot =ChatBot("Akushi", read_only=False,logic_adapters=[ {"import_path":"chatterbot.logic.BestMatch"
+                     "default_response" ,
+                     "Sorry I didn't understand"
+                     "maximum_similarity_threshold":0.9}]  )
+                     
+                    
+
+trainer=ChatterBotCorpusTrainer(bot)
+trainer.train("chatterbot.corpus.english")
+
+@app.route("/")
+def  main():
+    return render_template("index.html")
+
+
+#while True:
+    
+    #user_response=input("User : ") 
+    #print("Akushi : " +str(bot.get_response(user_response )))
+
+
+@app.route("/get")
+def get_chatbot_response():
+    userText = request.args.get('userMessage')
+    return str(bot.get_response(userText))
+    
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
